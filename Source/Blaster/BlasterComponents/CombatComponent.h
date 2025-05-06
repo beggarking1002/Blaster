4617fadff28764a0c0a46f50f18d53816090ca65
beggarking1002 <drake1002@naver.com>
2025-05-07 02:10:19 +0900

    Client_Side Predicting Reloading
    Client_Side Predicting Reloading

--- Source/Blaster/BlasterComponents/CombatComponent.h
+++ Source/Blaster/BlasterComponents/CombatComponent.h
@@ -48,7 +48,8 @@ public:
 	void ServerLaunchGrenade(const FVector_NetQuantize& Target);
 
 	void PickupAmmo(EWeaponType WeaponType, int32 AmmoAmount);
-	
+
+	bool bLocallyReloading = false;
 protected:
 	virtual void BeginPlay() override;
 	void SetAiming(bool bIsAiming);
